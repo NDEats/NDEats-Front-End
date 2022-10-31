1c77b07bcd059465816fc4585a4bb5bf7c294a53
Sunrise <Nana@weiyuns-mbp.dhcp.nd.edu>
2022-10-30 22:23:44 -0400

    Google LogIn Button

--- nd-eats-front/src/App.js
+++ nd-eats-front/src/App.js
@@ -1,25 +1,36 @@
+
 import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+import jwt_decode from 'jwt-decode';
+
 import './App.css';
 
 function App() {
+
+  function handleCallBackResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential)
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "236909895094-tk0l98a0nak3cq1ceidrk7iv2efo5978.apps.googleusercontent.com",
+      callback: handleCallBackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline", size: "large"}
+    )
+  }, []); //useEffect only runs again if [] changes
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id = "signInDiv"></div>
     </div>
   );
 }
 
 export default App;
+
+
