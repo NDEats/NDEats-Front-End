891366dad8351953413240432ebc838e7fe9b24c
Christopher Boumalhab <cboumalh@nd.edu>
2022-11-03 18:34:26 -0400

    can now add orders

--- nd-eats-front/src/components/AddOrder.js
+++ nd-eats-front/src/components/AddOrder.js
@@ -5,9 +5,31 @@ import Modal from 'react-bootstrap/Modal';
 
 function AddOrder() {
   const [show, setShow] = useState(false);
+  const [obj, setObj] = useState({'pickup': 'Duncan Student Center'});
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const handleChange = (e) => {
+    const currKey = e['target']['id'];
+    const currVal = e['target']['value'];
+    const addedKeyObj = obj;
+    addedKeyObj[currKey] = currVal;
+    setObj(addedKeyObj);
+  }
+
+  async function handleSubmit(obj){
+    const rawResponse = await fetch('http://127.0.0.1:8000/orders/', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(obj)
+  });
+    const content = await rawResponse.json();
+    handleClose();
+    return content;
+  }
 
   return (
     <>
@@ -21,18 +43,19 @@ function AddOrder() {
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlEmail">
+            <Form.Group className="mb-3" controlId="email">
               <Form.Label>Email address</Form.Label>
               <Form.Control
                 type="email"
                 placeholder="name@example.com"
                 autoFocus
+                onChange={handleChange}
               />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="exampleForm.Location">
+            <Form.Group className="mb-3" controlId="pickup">
               <Form.Label>Pickup</Form.Label>
-              <Form.Select>
+              <Form.Select onChange={handleChange}>
                   <option>Duncan Student Center</option>
                   <option>Lafortune Student Center</option>
                   <option>Hesburgh</option>
@@ -40,41 +63,44 @@ function AddOrder() {
               </Form.Select>
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="exampleForm.NbrItems">
+            <Form.Group className="mb-3" controlId="readyBy">
               <Form.Label>Ready By</Form.Label>
               <Form.Control
                 type="time"
                 min="0"
                 autoFocus
+                onChange={handleChange}
               />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="exampleForm.Pay">
+            <Form.Group className="mb-3" controlId="tip">
               <Form.Label>Tip</Form.Label>
               <Form.Control
                 min="0"
                 type="number"
                 placeholder="$#"
                 autoFocus
+                onChange={handleChange}
               />
             </Form.Group>
 
             <Form.Group
               className="mb-3"
-              controlId="exampleForm.Fullname"
+              controlId="dropoff"
             >
               <Form.Label>Dropoff</Form.Label>
               <Form.Control 
                 type="text"
                 placeholder="Duncan Hall"
+                onChange={handleChange}
               />
             </Form.Group>
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="success" onClick={handleClose}>
-            Submit
-          </Button>
+          <Button variant="success" type="submit" onClick={() => handleSubmit(obj)}>
+                Submit
+            </Button>
         </Modal.Footer>
       </Modal>
     </>
